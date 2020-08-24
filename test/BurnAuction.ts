36f53f89bdb70cc7f538c8c2abd019ba3208b8ff
snaketh4xor <snaaketh4xor@protonmail.com>
2020-08-24 12:15:27 +0530

    todo-logic changes for offchain pricing,remaining spec tests after logic

--- test/BurnAuction.ts
+++ test/BurnAuction.ts
@@ -8,6 +8,7 @@ let url = "http://127.0.0.1:8545";
 const provider = new ethers.providers.JsonRpcProvider(url);
 //console.log(provider);
 
+//contract tests
 contract("BurnAuction", async function(accounts) {
 	
 	let burnAuctionInstance: any;
@@ -15,52 +16,101 @@ contract("BurnAuction", async function(accounts) {
 	let CoordinatorB: any;
 	let CoordinatorC: any;
 	let blocknumber: number;
-	//let delay:number = 100;
-	//let genesisBlock:number;
+	//const delay:number = 100;
 	
 	before(async function() {
-		burnAuctionInstance = await BurnAuction.deployed();
+		//burnAuctionInstance = await BurnAuction.deployed();
+		//let tempGen = await burnAuctionInstance.genesisBlock();
+		//genesisblock = tempGen.toNumber();
 		//let initBlock = await provider.getBlockNumber();
 		//genesisBlock = initBlock + delay;
 		//console.log(genesisBlock);
 		});
 		
 	it('should deploy the BurnAuction Contract', async function() {
-		//console.log(burnAuctionInstance.address);
 		burnAuctionInstance = await BurnAuction.deployed();
+		//console.log(burnAuctionInstance.address);
 		assert(burnAuctionInstance.address !== '');
 	});
 	
-	it('should get slot id from blocknumber input', async function() {
-		burnAuctionInstance = await BurnAuction.deployed();
-		blocknumber = await provider.getBlockNumber();
-		console.log(blocknumber);
+	//complete after logic updates
+	/*
+	it('should ', async function() {
+		burnAuctionInstance = await BurnAuction.deployed();		
+		assert();
 	});
 	
-	it('should get slot id from blocknumber input', async function() {
+	it('should ', async function() {
 		burnAuctionInstance = await BurnAuction.deployed();
-		const currentblock = await burnAuctionInstance.getBlockNumber();
-		const genesisblock = await burnAuctionInstance.genesisBlock();
-		console.log(currentblock.toNumber());
-		console.log(genesisblock.toNumber());
+		assert();
 	});
 	
-	it('should get current blocknumber', async function() {
-        burnAuctionInstance = await BurnAuction.deployed();		
+	it('should ', async function() {
+		burnAuctionInstance = await BurnAuction.deployed();
+		assert();
+	});
+	*/
+	
+	it('should get current blocknumber', async function() {	
+	    burnAuctionInstance = await BurnAuction.deployed();
 	    //const currentblock = await burnAuctionInstance.getBlockNumber();
 		//const genesisblock = await burnAuctionInstance.genesisBlock();
 		//console.log(currentblock.toNumber());
 		//console.log(genesisblock.toNumber());
 		blocknumber = await provider.getBlockNumber();
 		//blocknumber = ethers.utils.bigNumberify(blocknumber);
-		const result = await burnAuctionInstance.getBlockNumber();
+		let result = await burnAuctionInstance.getBlockNumber();
 		//console.log(blocknumber);
 		//console.log(result);
 		//console.log(result.toString());
 		assert(result.toNumber() === blocknumber);
 	});
 	
-		
+	it('should get current slot number', async function() {
+		burnAuctionInstance = await BurnAuction.deployed();
+		burnAuctionInstance = await BurnAuction.deployed();
+		let tempGen = await burnAuctionInstance.genesisBlock();
+		let genesisblock = tempGen.toNumber();
+		let blocksperslot = await  burnAuctionInstance.blocksPerSlot();
+		let currentblock = await burnAuctionInstance.getBlockNumber();
+		currentblock = currentblock.toNumber();
+		//for weird truffle block differance
+		//currentblock = currentblock - 6;
+		console.log(currentblock);
+		let valueToCompare = (currentblock - genesisblock)/blocksperslot.toNumber();
+		let result = await burnAuctionInstance.currentSlot();
+		//console.log(result.toNumber());
+		//console.log(valueToCompare);
+		assert( result.toNumber() === valueToCompare );
+	});
 	
+	it('should get slot id from blocknumber', async function() {
+		//currentblock taken as argument for block number
+		burnAuctionInstance = await BurnAuction.deployed();
+		let tempGen = await burnAuctionInstance.genesisBlock();
+		let genesisblock = tempGen.toNumber();
+		let blocksperslot = await  burnAuctionInstance.blocksPerSlot();
+		let currentblock = await burnAuctionInstance.getBlockNumber();
+		currentblock = currentblock.toNumber();
+		//for weird truffle block differance
+		//currentblock = currentblock - 6;
+		let valueToCompare = (currentblock - genesisblock)/blocksperslot.toNumber();
+		let result = await burnAuctionInstance.block2slot(currentblock);
+		//console.log(result.toNumber());
+		//console.log(valueToCompare);
+		assert( result.toNumber() === valueToCompare );
+	});
+	
+	it('should get first blocknumber for given slot', async function() {
+		burnAuctionInstance = await BurnAuction.deployed();
+		let genesisblock = await burnAuctionInstance.genesisBlock();
+		let blocksperslot = await  burnAuctionInstance.blocksPerSlot();
+		let currentslot = await burnAuctionInstance.currentSlot();
+		let result = burnAuctionInstance.getBlockBySlot(currentslot);
+		let valueToCompare = currentslot.toNumber() * blocksperslot.toNumber() + genesisblock.toNumber();
+		console.log(valueToCompare);
+		console.log(result.toNumber());
+		assert( result.toNumber() === valueToCompare );
+	});
 	
 });
